34ac8eca4600734c8945095591d43f78d3784175
alial_99 <alasadi56@gmail.com>
2017-07-15 09:59:48 +0000

    athene: set proper dtbTool custom args
    Change-Id: I83111042272bd9ac9aa1257d76d79a550ee860b2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_CUSTOM_DTBTOOL := dtbTool_custom
+BOARD_DTBTOOL_ARGS := --force-v3 --motorola 1
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := athene_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8952
